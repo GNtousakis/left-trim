59665a34b3406053065112fabd390f0a1c6d30af
GNtousakis <greco1996@gmail.com>
2021-01-18 14:33:17 +0200

    Change NAN_METHOD

--- src/LeftTrim.cpp
+++ src/LeftTrim.cpp
@@ -9,19 +9,19 @@ using v8::String;
 using v8::Value;
 using v8::Handle;
 
-void LeftTrim(const FunctionCallbackInfo<Value>& args){
-  Isolate* isolate = args.GetIsolate();
+NAN_METHOD (LeftTrim) {
+  Isolate* isolate = info.GetIsolate();
   
   // Check the number of arguments passed.
-  if (args.Length() == 0 || !args[0]->IsString()) {
+  if (info.Length() == 0 || !info[0]->IsString()) {
     // Throw an Error that is passed back to JavaScript
     isolate->ThrowException(Exception::TypeError(
         String::NewFromUtf8(isolate, "You need to pass a string")));
     return;
   }
 
-  if (args[0]->IsString()) {
-    String::Utf8Value tmp(args[0]->ToString());
+  if (info[0]->IsString()) {
+    String::Utf8Value tmp(info[0]->ToString());
     // Change string to c++ type
     std::string str = std::string(*tmp); 
     std::string str_trim;
@@ -38,13 +38,14 @@ void LeftTrim(const FunctionCallbackInfo<Value>& args){
 
     // Revert to v8 string and return 
     v8::Local<v8::String> v8String = v8::String::NewFromUtf8(isolate, str_trim.c_str(), v8::String::kNormalString);
-    args.GetReturnValue().Set(v8String);
+    info.GetReturnValue().Set(v8String);
   }
 
 }
 
-void Init(Handle<Object> exports) {
-  NODE_SET_METHOD(exports, "LeftTrim", LeftTrim);
+NAN_MODULE_INIT(Init)
+{
+  NAN_EXPORT(target, LeftTrim);
 }
 
 NODE_MODULE(LeftTrim, Init)
